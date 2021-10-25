3124688ff96f5456b52f996b32054394120b2eb0
rexdougie <bitokdouglas@gmail.com>
2021-10-25 15:00:26 +0100

    Profile Page Set

--- screens/Home/Profile.js
+++ screens/Home/Profile.js
@@ -4,7 +4,8 @@ import {
     Text,
     TouchableOpacity
 } from 'react-native';
-import { COLORS } from '../../constants';
+import { COLORS, FONTS, SIZES } from '../../constants';
+import { styles } from '../../styles';
 
 const Profile = ({ route, navigation }) => {
     
@@ -16,14 +17,65 @@ const Profile = ({ route, navigation }) => {
     //     year,
     // } = route.params;
 
+    function renderHeader() {
+        return (
+            <View style={{
+                justifyContent: 'center',
+            }}>
+                <View style={{
+                    top: 50,
+                    paddingLeft: SIZES.padding * 2,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}>
+                    <View style={{
+                        // top: 10,
+                        backgroundColor: COLORS.lightGray3,
+                        borderRadius: 16,
+                        height: 50,
+                        paddingHorizontal: SIZES.padding,
+                        ...styles.shadows,
+                        alignItems: 'flex-end',
+                        justifyContent: 'center'
+                    }}>
+                        <Text style={{
+                            ...FONTS.h1,
+                        }}>
+                            Profile Page
+                        </Text>
+                    </View>
+                </View>
+                <TouchableOpacity style={{
+                    backgroundColor: COLORS.blue,
+                    height: 36,
+                    borderRadius: 16,
+                    margin: 10,
+                    width: 64,
+                    ...styles.shadows,
+                    alignItems: 'flex-start'
+                }}
+                    onPress={() => {navigation.goBack()}}
+                >
+                    <Text style={{
+                        ...FONTS.h2,
+                        color: COLORS.white,
+                        top: -7,
+                        padding: SIZES.padding / 2
+                    }}>Back</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+
     return (
         <View
             style={{
                 flex: 1,
-                alignItems: 'center',
-                justifyContent: 'center'
+                // alignItems: 'center',
+                // justifyContent: 'center'
             }}
         >
+            {renderHeader()}
         <View
             style={{
                 flex: 1,
@@ -54,14 +106,14 @@ const Profile = ({ route, navigation }) => {
                 yearDOB :
                  {/* {year} */}
             </Text>
-        <TouchableOpacity style={{
+        {/* <TouchableOpacity style={{
             backgroundColor: COLORS.black,
             height: 25
         }}
             onPress={() => {navigation.navigate('BMI')}}
         >
             <Text>Home</Text>
-        </TouchableOpacity>
+        </TouchableOpacity> */}
         </View>
         </View>
     )
